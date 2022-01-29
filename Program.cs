86051e7d989c844cf25a193f96ba50a3a7efdefd
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-01-29 19:36:58 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,55 @@ namespace Udemyyyyy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.Write("Enter year - ");
+		    int year = int.Parse(Console.ReadLine());
+		
+		    if(year == 1)
+		    {
+		    	Console.WriteLine("Freshman");
+		    }
+		     else if(year == 2)
+		    {
+			    Console.WriteLine("Sophomore");
+		    }
+		    else if(year == 3)
+		    {
+		    	Console.WriteLine("Junior");
+		    }   
+		    else if(year == 4)
+		    {
+		    	Console.WriteLine("Senior");
+		    }
+            
+	//switch statement	
+		    switch(year)
+		    {
+			    case 1:
+			   	{
+					Console.WriteLine("Freshman");
+					break;
+				}
+			    case 2:
+				{
+					Console.WriteLine("Sophomore");
+					break;
+				}
+			    case 3:
+				{
+					Console.WriteLine("Junior");
+					break;
+				}
+			    case 4:
+				
+				{
+					Console.WriteLine("Senior");	
+					break;
+				}
+			    default:
+				{
+					Console.WriteLine("Invalid year");
+					break;
+				}
+		   }
     }
 }
